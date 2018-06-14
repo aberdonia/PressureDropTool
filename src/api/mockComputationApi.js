3aa38201e77b148720c409c48c8ea8aef73ef1b3
Jamie McGee <jamo92@hotmail.co.uk>
2018-06-14 20:59:42 +0100

    Colebroke calculation goalseeking, if statement to determine friction factor.

--- src/api/mockComputationApi.js
+++ src/api/mockComputationApi.js
@@ -3,51 +3,50 @@ import delay from './delay';
 var nr = require('newton-raphson-method');
 
 const contants =
-{
-      x: 17,
-      y: 2
-};
+  {
+    x: 17,
+    y: 2
+  };
 
 const inputs =
-{
+  {
     volumetric_expansion: 0,
     C_Value_Threshold_for_Erosion: 125,
     required_flowrate: 0.5, //m3/hr
     density: 1070, //kg/m3
     viscosity: 6.98 //cP
-};
+  };
 
 const data =
-{
+  {
     Re_laminar_max: 2000,
     Re_transitional_max: 4000,
-};
+  };
 
 // Re	Flow Regime
 // 0	Laminar
 // 2000	Transitional
 // 4000	Turbulent
 
-  
-  
-  class ComputationApi {
-    static getAllAuthors() {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve(Object.assign([], authors));
-        }, delay);
-      });
-    }
-  
-    static computePipe(pipes) {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
+
+class ComputationApi {
+  static getAllAuthors() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(Object.assign([], authors));
+      }, delay);
+    });
+  }
+
+  static computePipe(pipes) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
 
 
         let pipe_length = [];
-        let cummulative_length = [];
+        let cumulative_length = [];
         let mean_diameter = [];
-        let NonDimensional_Roughness = [];
+        let nonDimensional_Roughness = [];
         let fluid_rate = [];
         let fluid_veloicty = [];
         let reynolds = [];
@@ -56,46 +55,49 @@ const data =
         let friction_factor = [];
 
         for (let i = 0; i < pipes.length; i++) {
-            pipe_length.push(Math.sqrt(Math.pow(pipes[i].horizontal_change,2) + Math.pow(pipes[i].vertical_change,2) ));
-            mean_diameter.push(pipes[i].inner_diamter * Math.pow(1+(inputs.volumetric_expansion/100),0.5));
-            NonDimensional_Roughness.push(pipes[i].roughness/mean_diameter[i]);
-            fluid_rate.push(inputs.required_flowrate/3600/pipes[i].cores);
-            fluid_veloicty.push(fluid_rate[i]/(Math.PI*Math.pow((mean_diameter[i]/1000/2),2)));
-            reynolds.push((inputs.density*fluid_veloicty[i]*mean_diameter[i]/1000)/(inputs.viscosity/1000));
-            
-            // switch to chosen fluid model: laminar, transitional, turbulent
-            if (reynolds[i] < data.Re_laminar_max) {
-                correlation.push("Laminar");
-                friction_factor.push(64/reynolds[i]);
-            } else if (reynolds[i] >= data.Re_laminar_max && reynolds[i] < data.Re_transitional_max) {
-                correlation.push("Transitional");
-                friction_factor.push(64/reynolds[i]);
-                //TODO goalseeking for colebrook
-                
-            } else {
-                correlation.push("Turbulent");
-            }
+          pipe_length.push(Math.sqrt(Math.pow(pipes[i].horizontal_change, 2) + Math.pow(pipes[i].vertical_change, 2)));
+          mean_diameter.push(pipes[i].inner_diamter * Math.pow(1 + (inputs.volumetric_expansion / 100), 0.5));
+          nonDimensional_Roughness.push(pipes[i].roughness / mean_diameter[i]);
+          fluid_rate.push(inputs.required_flowrate / 3600 / pipes[i].cores);
+          fluid_veloicty.push(fluid_rate[i] / (Math.PI * Math.pow((mean_diameter[i] / 1000 / 2), 2)));
+          reynolds.push((inputs.density * fluid_veloicty[i] * mean_diameter[i] / 1000) / (inputs.viscosity / 1000));
 
-            // not sure if neccessary
-            if (i == 0) {
-                cummulative_length.push(pipe_length[0]);
-            } else{
-                cummulative_length.push(cummulative_length[i-1]+pipe_length[i]);
-            }
+          // switch to chosen fluid model: laminar, transitional, turbulent
+          const ff_laminar = 64 / reynolds[i];
+          const ff_colebrook = ComputationApi.colebrookFrictionCoefficient(nonDimensional_Roughness[i], reynolds[i]);
+          if (reynolds[i] < data.Re_laminar_max) {
+            correlation.push("Laminar");
+            friction_factor.push(ff_laminar);
+          } else if (reynolds[i] >= data.Re_laminar_max && reynolds[i] < data.Re_transitional_max) {
+            correlation.push("Transitional");
+
+            const ff_transitional = (reynolds[i] - data.Re_laminar_max) * (ff_colebrook - ff_laminar) / (data.Re_transitional_max - data.Re_laminar_max) + ff_laminar;
+            friction_factor.push(ff_transitional);
+          } else {
+            correlation.push("Turbulent");
+            friction_factor.push(ff_colebrook);
+          }
+
+          // not sure if neccessary
+          if (i === 0) {
+            cumulative_length.push(pipe_length[0]);
+          } else {
+            cumulative_length.push(cumulative_length[i - 1] + pipe_length[i]);
+          }
         }
 
         debugger;
         // Validated
         console.log(pipe_length);
         // Validated
-        console.log("cummulative_length:");
-        console.log(cummulative_length);
+        console.log("cumulative_length:");
+        console.log(cumulative_length);
         // Validated (same as inner_diameter, excel is rounding)
         console.log("mean diameter");
         console.log(mean_diameter);
         // Validated
-        console.log("NonDimensional_Roughness");
-        console.log(NonDimensional_Roughness);
+        console.log("nonDimensional_Roughness");
+        console.log(nonDimensional_Roughness);
         // Validated
         console.log("fluid_rate");
         console.log(fluid_rate);
@@ -108,26 +110,35 @@ const data =
         // V
         console.log("correlation");
         console.log(correlation);
-        
+        //
+        console.log("friction_factor");
+        console.log(friction_factor);
 
 
         resolve(pipes);
-        }, delay);
-      });
-    }
-  
-    static deleteAuthor(authorId) {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          const indexOfAuthorToDelete = authors.findIndex(author => {
-            author.id == authorId;
-          });
-          authors.splice(indexOfAuthorToDelete, 1);
-          resolve();
-        }, delay);
-      });
-    }
+      }, delay);
+    });
   }
-  
-  export default ComputationApi;
-  
+
+  static colebrookFrictionCoefficient(nonDimRoughness, reynolds) {
+    function f(x) {
+      return -2 * Math.log10(nonDimRoughness / 3.7 + 2.51 / (reynolds * Math.sqrt(x))) - 1 / Math.sqrt(x);
+    }
+
+    return nr(f, 0.01);
+  }
+
+  static deleteAuthor(authorId) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const indexOfAuthorToDelete = authors.findIndex(author => {
+          author.id == authorId;
+        });
+        authors.splice(indexOfAuthorToDelete, 1);
+        resolve();
+      }, delay);
+    });
+  }
+}
+
+export default ComputationApi;
